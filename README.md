dcd83d5e8bdeb46f685e7c1703bc2957081ef5bb
hdj <hdj@addcn.com>
2021-03-02 08:10:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Swoole HOOK Addon
+
+Make some programs compatible with Swoole HOOK. forked [sy-records/swoole-hook-addon](https://github.com/sy-records/swoole-hook-addon)
+
+## SWOOLE_HOOK_SOCKETS
+
+- [x] [Monolog](https://github.com/huangdijia/swoole-hook-addon/blob/master/src/Monolog.php)
---
+++ src/Monolog.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of swoole-hook-addon.
+ *
+ * @link     https://github.com/huangdijia/swoole-hook-addon
+ * @document https://github.com/huangdijia/swoole-hook-addon/-/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace Monolog\Handler\SyslogUdp;
+
+function is_resource($resource)
+{
+    if (defined('SWOOLE_HOOK_SOCKETS') && (\Swoole\Runtime::getHookFlags() & SWOOLE_HOOK_SOCKETS)) {
+        return \is_resource($resource) || $resource instanceof \Swoole\Coroutine\Socket;
+    }
+
+    return \is_resource($resource);
+}
