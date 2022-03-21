09bd3ba4711333fa648eb6be7c61bc749de66b35
D.J.Hwang <huangdijia@gmail.com>
2022-03-21 10:35:05 +0800

    Optimized

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,11 +4,11 @@ $header = <<<'EOF'
 This file is part of swoole-hook-addon.
 
 @link     https://github.com/huangdijia/swoole-hook-addon
-@document https://github.com/huangdijia/swoole-hook-addon/blob/main/README.md
+@document https://github.com/huangdijia/swoole-hook-addon/blob/master/README.md
 @contact  huangdijia@gmail.com
 EOF;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
@@ -16,7 +16,7 @@ return PhpCsFixer\Config::create()
         '@DoctrineAnnotation' => true,
         '@PhpCsFixer' => true,
         'header_comment' => [
-            'commentType' => 'PHPDoc',
+            'comment_type' => 'PHPDoc',
             'header' => $header,
             'separate' => 'none',
             'location' => 'after_declare_strict',
@@ -82,6 +82,7 @@ return PhpCsFixer\Config::create()
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
+            ->exclude('bin')
             ->exclude('public')
             ->exclude('runtime')
             ->exclude('vendor')
